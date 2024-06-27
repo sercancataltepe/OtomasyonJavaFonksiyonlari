3d287c0dfc44e440c493086df7a2905c28f46bfd
BITES\sercan.cataltepe <sercancataltepe91@gmail.com>
2024-06-27 13:44:00 +0300

    fourth commit

---
+++ src/ArabaStatic.java
@@ -0,0 +1,12 @@
+public class ArabaStatic {
+    private String model;
+    public static int arabaFiyati = 450000;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sercan Cataltepe");
+        System.out.println("----------------");
+
+        ArabaStatic ilkArabam = new ArabaStatic();
+        ilkArabam.setModel("Peugeot 207");
+        System.out.println("İlk Arabamin Modeli: " + ilkArabam.getModel() + " ve Fiyati: " + ArabaStatic.arabaFiyati);
+        ArabaStatic.arabaFiyati = 970000;
+        System.out.println("İlk Arabamin Modeli: " + ilkArabam.getModel() + " ve Yeni Fiyati: " + ArabaStatic.arabaFiyati);
+
+
+        ArabaStatic ikinciArabam = new ArabaStatic();
+        ikinciArabam.setModel("Volvo V40");
+        System.out.println("Ikinci Arabamın Modeli: " +ikinciArabam.getModel() + " ve Fiyati: " + ArabaStatic.arabaFiyati);
+        ArabaStatic.arabaFiyati = 1530000;
+        System.out.println("ikinci Arabamin Modeli: "+ ikinciArabam.getModel() + " ve Yeni Fiyati: " + ArabaStatic.arabaFiyati);
+
+        ArabaStatic ucuncuArabam = new ArabaStatic();
+        ucuncuArabam.setModel("BMW 4.28i Grandcoupe");
+        System.out.println("Ucuncu Arabamin  Modeli: " + ucuncuArabam.getModel() + " ve Fiyati: " + ArabaStatic.arabaFiyati);
+
+
+
         }
     }
